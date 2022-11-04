2a75b2dc442122c21ce1c77e3f6a1a4fbc980e06
Léo Duggan <leo.duggan@alumni.epfl.ch>
2022-11-04 16:55:42 +0100

    Replaced with inverted pendulum files

--- controllers/edit_me/edit_me.py
+++ controllers/edit_me/edit_me.py
@@ -1,23 +1,63 @@
-"""Simple robot controller."""
+"""Sample Webots controller for the inverted pendulum benchmark."""
 
 from controller import Robot
-import sys
-
-# Define the target motor position in radians.
-target = 11.9
+import math
 
 # Get pointer to the robot.
 robot = Robot()
 
-# Print the program output on the console
-print("Move the motors of the Thymio II to position " + str(target) + ".")
+# Get the time step of the current world.
+timestep = int(robot.getBasicTimeStep())
 
-# Set the target position of the left and right wheels motors.
-robot.getDevice("motor.left").setPosition(target)
-robot.getDevice("motor.right").setPosition(target)
+# Get pointers to the position sensor and enable it.
+ps = robot.getDevice('pendulum sensor')
+ps.enable(timestep)
 
-# This is the most simple controller that works for this benchmark
-# If you want to experiment with more complex functions, you can read the programming guide here:
-#  https://www.cyberbotics.com/doc/guide/controller-programming?tab-language=python
-# or the Robot() documentation here:
-#  https://cyberbotics.com/doc/reference/robot?tab-language=python
+# Get pointers to the motors and set target position to infinity (speed control).
+leftMotor = robot.getDevice("left wheel motor")
+rightMotor = robot.getDevice("right wheel motor")
+leftMotor.setPosition(float('+inf'))
+rightMotor.setPosition(float('+inf'))
+leftMotor.setVelocity(0.0)
+rightMotor.setVelocity(0.0)
+maxSpeed = min(rightMotor.getMaxVelocity(), leftMotor.getMaxVelocity())
+
+# Define the PID control constants and variables.
+KP = 31.4
+KI = 100.5
+KD = 0
+integral = 0.0
+previous_position = 0.0
+
+# Initialize the robot speed (left wheel, right wheel).
+leftMotor.setVelocity(0.0)
+rightMotor.setVelocity(0.0)
+
+# Main loop: perform a simulation step until the simulation is over.
+while robot.step(timestep) != -1:
+    # Read the sensor measurement.
+    position = ps.getValue()
+
+    # Stop the robot when the pendulum falls.
+    if math.fabs(position) > math.pi * 0.5:
+        leftMotor.setVelocity(0.0)
+        rightMotor.setVelocity(0.0)
+        break
+
+    # PID control.
+    integral = integral + (position + previous_position) * 0.5 / timestep
+    derivative = (position - previous_position) / timestep
+    speed = KP * position + KI * integral + KD * derivative
+
+    # Clamp speed to the maximum speed.
+    if speed > maxSpeed:
+        speed = maxSpeed
+    elif speed < -maxSpeed:
+        speed = -maxSpeed
+
+    # Set the robot speed (left wheel, right wheel).
+    leftMotor.setVelocity(-speed)
+    rightMotor.setVelocity(-speed)
+
+    # Store previous position for the next controller step.
+    previous_position = position
--- controllers/supervisor/supervisor.py
+++ controllers/supervisor/supervisor.py
@@ -1,50 +1,77 @@
-"""Supervisor of the Robot Programming benchmark."""
+"""Controller program to manage the benchmark.
+
+It manages the perturbation and evaluates the performance of the user
+controller.
+"""
 
 from controller import Supervisor
 import os
+import random
 import sys
+import time as timeLib
 
-# Constant used for the automated benchmark evaluation script
-# - can also be used to generate an animation in storage folder if set to True
+# set to True if you want to generate an animation of your controller to ./storage/local
 RECORD_ANIMATION = False
 
-if RECORD_ANIMATION:
-    import recorder.recorder as rec
+import recorder.recorder as rec
 
-def benchmarkPerformance(message):
+def benchmarkPerformance(message, robot):
     benchmark_name = message.split(':')[1]
     benchmark_performance_string = message.split(':')[3]
     print(benchmark_name + ' Benchmark complete! Your performance was ' + benchmark_performance_string)
 
-supervisor = Supervisor()
+# Get random generator seed value from 'controllerArgs' field
+seed = 1
+if len(sys.argv) > 1 and sys.argv[1].startswith('seed='):
+    seed = int(sys.argv[1].split('=')[1])
 
-timestep = int(supervisor.getBasicTimeStep())
+robot = Supervisor()
 
-thymio = supervisor.getFromDef("BENCHMARK_ROBOT")
-translation = thymio.getField("translation")
+timestep = int(robot.getBasicTimeStep())
+
+jointParameters = robot.getFromDef("PENDULUM_PARAMETERS")
+positionField = jointParameters.getField("position")
+
+# emitter needed for the physics plugin?
+emitter = robot.getDevice("emitter")
+time = 0
+force = 0
+forceStep = 800
+random.seed(seed)
+running = True
 
 if RECORD_ANIMATION:
     # Recorder code: wait for the controller to connect and start the animation
-    rec.animation_start_and_connection_wait(supervisor)
+    rec.animation_start_and_connection_wait(robot)
     step_max = 1000 * rec.MAX_DURATION / timestep
     step_counter = 0
 
-tx = 0
-running = True
-while supervisor.step(timestep) != -1 and running:
-    t = translation.getSFVec3f()
+while robot.step(timestep) != -1 and running:
     if running:
-        percent = 1 - abs(0.25 + t[0]) / 0.25
-        if percent < 0:
-            percent = 0
-        if t[0] < -0.01 and abs(t[0] - tx) < 0.0001:  # away from starting position and not moving any more
+        time = robot.getTime()
+        robot.wwiSendText("time:%-24.3f" % time)
+        robot.wwiSendText("force:%.2f" % force)
+
+        # Detect status of inverted pendulum
+        position = positionField.getSFFloat()
+        if position < -1.58 or position > 1.58:
+            # stop
             running = False
-            name = 'Robot Programming'
-            message = f'success:{name}:{percent}:{percent*100:.2f}%'
+            name = "Inverted Pendulum"
+            performance = str(time)
+            performanceString = rec.time_convert(time)
+            message = 'success:' + name + ':' + performance + ':' + performanceString
+            robot.wwiSendText(message)
         else:
-            message = f"percent:{percent}"
-        supervisor.wwiSendText(message)
-        tx = t[0]
+            if forceStep <= 0:
+                forceStep = 800 + random.randint(0, 400)
+                force = force + 0.02
+                toSend = "%.2lf %d" % (force, seed)
+                if sys.version_info.major > 2:
+                    toSend = bytes(toSend, "utf-8")
+                emitter.send(toSend)
+            else:
+                forceStep = forceStep - 1
     if RECORD_ANIMATION:
         # Stops the simulation if the controller takes too much time
         step_counter += 1
@@ -53,10 +80,10 @@ while supervisor.step(timestep) != -1 and running:
 
 if RECORD_ANIMATION:
     # Write performance to file, stop recording and close Webots
-    rec.record_performance(running, percent)
-    rec.animation_stop(supervisor, timestep)
-    supervisor.simulationQuit(0)
+    rec.record_performance(running, time)
+    rec.animation_stop(robot, timestep)
+    robot.simulationQuit(0)
 else:
-    benchmarkPerformance(message)
+    benchmarkPerformance(message, robot)
 
-supervisor.simulationSetMode(Supervisor.SIMULATION_MODE_PAUSE)
+robot.simulationSetMode(Supervisor.SIMULATION_MODE_PAUSE)
